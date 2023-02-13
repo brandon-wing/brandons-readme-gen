8f316ba890b731ddfe9f610419400928502cb229
rudyxwhite <brandonrwing98@gmail.com>
2023-02-13 15:25:52 -0600

    created questions + answers for the readme. Created a function to initialize the prompts.

--- Develop/index.js
+++ Develop/index.js
@@ -1,65 +1,69 @@
 // TODO: Include packages needed for this application
  const fs = require('fs')
  const inquirer = require('inquirer')
+ const markdown = require('./utils/generateMarkdown')
 // TODO: Create an array of questions for user input
-const questions = [
+
+
+const userQuestions = [
     {
         type: 'input',
-        message: '',
-        name:''
+        message: 'What is the title of your project?',
+        name:'title'
     },
     {
         type: 'input',
-        message: '',
-        name:''
+        message: 'Provide a short description explaining the what, why, and how of your project.',
+        name:'description'
     },
     {
         type: 'input',
-        message: '',
-        name:''
+        message: 'What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.',
+        name:'installation process'
     },
     {
         type: 'input',
-        message: '',
-        name:''
+        message: 'Provide instructions and examples for use. Include screenshots as needed.',
+        name:'usage'
     },
     {
         type: 'input',
-        message: '',
-        name:''
+        message: 'Please provide a screenshot from your assets folder in the correct format: "![alt text](assets/images/screenshot.png)"',
+        name:'screenshot'
     },
     {
         type: 'input',
-        message: '',
-        name:''
+        message:'Please enter your github username.',
+        name:'username'
     },
     {
         type: 'input',
-        message: '',
-        name:''
+        message: 'Please type the link to your repository.',
+        name:'repo'
     },
     {
         type: 'input',
-        message: '',
-        name:''
+        message: 'Please enter any necessary contact information, such as email or a link to social media account.',
+        name:'contact'
     },
     {
-        type: 'input',
-        message: '',
-        name:''
-    },    {
-        type: 'input',
-        message: '',
-        name:''
-    }
+        type: 'checkbox',
+        message: 'Please select the license used for this project.',
+        name:'license',
+        choices: ['MIT', 'W3C', 'ISC', 'AAL', 'NTP', 'No license required!']
+    },
+
 ];
 
+
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+
+}
 
 // TODO: Create a function to initialize app
 function init() {
-    
+    inquirer.prompt(userQuestions)
 }
 
 // Function call to initialize app
